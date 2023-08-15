d762ddba294643a442068662897f98abee83c226
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-15 11:14:11 -0700

    pets show route

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,11 @@
 class PetsController < ApplicationController
   def index
     @pets = Pet.all
+    render :index
+  end
+
+  def show
+    @pet = Pet.find_by(id: params[:id])
+    render :show
   end
 end
