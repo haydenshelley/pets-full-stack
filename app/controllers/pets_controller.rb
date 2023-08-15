9b8a1a4564e58476bccffb429e0b030bb348fd6d
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-15 11:38:04 -0700

    pets new and create routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,18 @@ class PetsController < ApplicationController
     @pet = Pet.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @pet = Pet.new
+    render :new
+  end
+
+  def create
+    @pet = Pet.create(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image]
+    )
+    redirect_to "/pets"
+  end
 end
